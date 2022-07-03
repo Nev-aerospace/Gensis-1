ef503b1b231c7926148d9ce7976e31fba889990d
StoneMcYT <67421882+StoneMcYT@users.noreply.github.com>
2022-07-03 18:48:55 +0800

    Update main.py
    Fix syntac and comment out twillo lines

--- main/main.py
+++ main/main.py
@@ -5,14 +5,14 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
-##############################################
-# Twilio MQTT Demo for Programmable Wireless #
-##############################################
-from time import sleep
-from sys import exit
-from smbus import SMBus
-import json
-import paho.mqtt.client as mqtt
+# ##############################################
+# # Twilio MQTT Demo for Programmable Wireless #
+# ##############################################
+# from time import sleep
+# from sys import exit
+# from smbus import SMBus
+# import json
+# import paho.mqtt.client as mqtt
 
 
 #Cpu Temp Checker
@@ -39,11 +39,11 @@ client = Client(account_sid, auth_token)
 
 #INtro
 print("Welcome to", model + "Python bassed control module")
-print("
+
 #CPU Print
 print(cpu.temperature)
 #Sense hat wake up
-sense.show.message("HAT is connected)
+sense.show.message("HAT is connected")
                    
                    
              
